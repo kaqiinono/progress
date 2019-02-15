aa42d1365c78e6f0fe10067bd864bdb7dfc0e7c8
meinuo <Smn848789>
2019-02-15 18:48:39 +0800

    Merge branch 'master' into gh-pages
    # Conflicts:
    #	.gitignore
    #	.travis.yml
    #	HISTORY.md
    #	README.md
    #	assets/index.less
    #	examples/fast-progress.html
    #	examples/fast-progress.js
    #	examples/gap.html
    #	examples/gap.js
    #	examples/simple.js
    #	index.js
    #	package.json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import Line from './Line';
 import Circle from './Circle';
+import Rect from './Rect';
 
 export {
   Line,
   Circle,
+  Rect,
 };
 
 export default {
   Line,
   Circle,
+  Rect,
 };
