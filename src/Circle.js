1a891afdd706fc76822690d66af983d39d8cd6af
meinuo <Smn848789>
2019-02-15 14:54:51 +0800

    Merge branch 'master' into gh-pages
    # Conflicts:
    #	.gitignore
    #	.travis.yml
    #	HISTORY.md
    #	README.md
    #	assets/index.less
    #	examples/fast-progress.html
    #	examples/fast-progress.js
    #	examples/gap.html
    #	examples/gap.js
    #	examples/simple.js
    #	index.js
    #	package.json

--- src/Circle.js
+++ src/Circle.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import enhancer from './enhancer';
 import { propTypes, defaultProps } from './types';
+import '../assets/index.less';
 
 class Circle extends Component {
   getPathStyles(offset, percent, strokeColor, pathWidth, gapDegree = 0, gapPosition) {
