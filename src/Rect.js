7d4e0365313e6e469469d917c35e18ca0740eb7e
meinuo <Smn848789>
2019-02-18 16:41:01 +0800

    rect 添加渐变颜色，不受颜色数量限制

--- src/Rect.js
+++ src/Rect.js
@@ -16,7 +16,7 @@ class Rect extends Component {
       rectHeight,
     } = this.props;
 
-    const strokeColor = this.props.strokeColor || 'url("#gradient")';
+    const strokeColor = this.props.strokeColor || `url("#gradient${gradientColor.length}")`;
 
 
     return (
@@ -25,10 +25,20 @@ class Rect extends Component {
         style={style}
       >
         <defs>
-          <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-            <stop offset="0%" style={{ stopColor: gradientColor[0], stopOpacity: 1 }}></stop>
-            <stop offset="100%" style={{ stopColor: gradientColor[1], stopOpacity: 1 }}></stop>
-          </linearGradient>
+          {
+            gradientColor.length > 3 &&
+            <linearGradient id={`gradient${gradientColor.length}`} x1="0%" y1="0%" x2="100%" y2="0%">
+              {
+                gradientColor.map((color, index) => {
+                  if (index !== gradientColor.length - 1) {
+                    const offset = `${Math.round(100 / gradientColor.length) * index}%`;
+                    return <stop offset={offset} style={{ stopColor: color, stopOpacity: 1 }}></stop>;
+                  }
+                  return <stop offset="100%" style={{ stopColor: color, stopOpacity: 1 }}></stop>;
+                })
+              }
+            </linearGradient>
+          }
         </defs>
         <rect
           width={rectWidth}
